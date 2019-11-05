3da65dd4542f652013eaac7284cf9a111beb45d9
Qantrous <ikant14@freeuni.edu.ge>
2019-11-05 11:31:18 +0400

    first commit

---
+++ src/main/java/ge/omedia/pin/PinApplication.java
@@ -0,0 +1,13 @@
+package ge.omedia.pin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ge/omedia/pin/PinApplicationTests.java
@@ -0,0 +1,13 @@
+package ge.omedia.pin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
